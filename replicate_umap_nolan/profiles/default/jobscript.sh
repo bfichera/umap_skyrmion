f63830a2cce9cf60964e00dec72a7e9ea421b5bc
Bryan Fichera <bfichera@anl.gov>
2026-02-24 14:16:50 -0600

    Switch to snakemake job submission

--- replicate_umap_nolan/profiles/default/jobscript.sh
+++ replicate_umap_nolan/profiles/default/jobscript.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#PBS -l walltime=14400
-#PBS -l nodes=1:ppn=${snakemake[threads]}:gen8
 #PBS -A cnm83860
-
+#PBS -l walltime={resources.walltime}
+#PBS -l nodes=1:ppn={threads}:{resources.arch}
 #PBS -m ea
 
 # change into the directory where qsub will be executed
@@ -15,6 +14,6 @@ module load gcc
 export NUMBA_NUM_THREADS=$PBS_NP
 export MKL_NUM_THREADS=$PBS_NP
 export OMP_NUM_THREADS=$PBS_NP
-
 export NUMBA_THREADING_LAYER=workqueue
-envs/venv/bin/python ${snakemake_params[code]}
+
+{exec job}
